230504f3963bdd87ba252637f5f8e724cc4de0bf
Luca Delucchi <lucadeluge@gmail.com>
2011-12-04 18:22:40 +0100

    add support to pbf files

--- other_nation.sh
+++ other_nation.sh
@@ -17,11 +17,13 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-$NO_ARG=0
+NO_ARG=0
 ###  FUNZIONE PER L'HELP ##
 usage()
 {
-  echo "Utilizzo: `basename $0` file.osm.bz2 "
+  echo "Utilizzo: `basename $0` file.osm.bz2 
+
+"
 
 }
 #controlla se non ci sono parametri ed stampa l'help
@@ -33,7 +35,7 @@ fi
 
 file_in=$1;
 name_nation=`echo $1 | cut -d'.' -f'1';`
-
+prefix=`echo ${1##*.}`
 
 #####VARIABILI DA SETTARE#####
 #nome della zona rappresentata 
@@ -41,8 +43,8 @@ name=$name_nation
 #abbreviazione della zona
 #abbr="IT"
 #nome dello style
-style_it="../resources/styles/gfoss"
-style="../../../resources/styles/gfoss"
+style_it="../../styles/gfoss"
+style_escu="../../styles/hiking"
 
 #nome della mappa
 serie=${name_nation}" creata da ital.img"
@@ -50,36 +52,32 @@ serie=${name_nation}" creata da ital.img"
 priority="10"
 
 ##### SCRIP #####
-bzcat $file_in > ${name_nation}.osm
+
+if [ "$prefix" = bz ] ; then
+
+  bzcat $file_in > ${name_nation}.osm
+
+fi
 
 ### CREA IL FILE DELLA NAZIONE ###
 #divide il file osm, se si cambia regione ricordarsi di cambiare il nome
-java -Xmx1000M -jar splitter.jar --overlap=2000 ${name_nation}.osm
-#crea il file .img dei singoli pezzi della nazione
-for NAME in $(find *.osm.gz -type f)
-do
-        gzip -d $NAME
-        nome=`echo "$NAME" | cut -d'.' -f1`
-        filename=${nome}/${nome}.img" "
-        stringa=${stringa}${filename} 
-        mkdir $nome
-        cd $nome
-        java -Xmx1000M -jar ../mkgmap.jar --style-file=$style_it --net --route --latin1 --country-name="$name" --draw-priority=$priority --add-pois-to-areas --series-name="$serie" ../$nome.osm  #--style-file=$style
-        cd ..
-
-done
-
-echo $stringa
-#unisce tutti i file in un unico file della nazione
-java -Xmx1000M -jar mkgmap.jar --gmapsupp $stringa 
-#comprime il file
-tar -cf ${name_nation}.tar gmapsupp.img README_gfoss.txt 
-gzip -9 -f ${name_nation}.tar
+if [ "$prefix" = bz ] ; then
+  java -Xmx2500M -jar splitter-r180/splitter.jar --overlap=2000 ${name_nation}.osm
+else
+  java -Xmx2500M -jar splitter-r180/splitter.jar --overlap=2000 $file_in
+fi
+cd tmp/nations
+#crea il file img
+java -Xmx2000M -jar ../../mkgmap-r1995/mkgmap.jar --style-file=$style_it --net --route --latin1 --country-name="$name" --country-abbr="$abbr" --draw-priority=$priority --add-pois-to-areas --series-name="$serie" ../../6*.osm.pbf  #--style-file=$style
+java -Xmx1000M -jar ../../mkgmap-r1995/mkgmap.jar --gmapsupp *.img
+tar -cf ../../output_img/${name_nation}.tar gmapsupp.img ../../README_data.txt 
+gzip -9 -f ../../output_img/${name_nation}.tar
+cd ../../
+rm -rf tmp/nations*
 
 #rimuove tutti i file non più utili
 rm -rf 632400*
 rm -rf *.img 
 rm -rf *.IMG 
-rm -rf *.osm
-rm -rf regioni/*
+# rm -rf *.osm
  
