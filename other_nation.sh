bc568ed7fd6eff96b4d5d805a91cae457ef0ea5f
Luca Delucchi <lucadeluge@gmail.com>
2012-05-22 18:07:24 +0200

    fix other_nation.sh

--- other_nation.sh
+++ other_nation.sh
@@ -21,8 +21,11 @@ NO_ARG=0
 ###  FUNZIONE PER L'HELP ##
 usage()
 {
-  echo "Utilizzo: `basename $0` file.osm.bz2 
+  echo "Utilizzo: `basename $0` opzioni file.osm.bz2/pbf
 
+Opzioni: 
+    -o          osm file not compressed
+    -p          scarica/usa file pbf
 "
 
 }
@@ -45,35 +48,57 @@ name=$name_nation
 #nome dello style
 style_it="../../styles/gfoss"
 style_escu="../../styles/hiking"
-
+mkgmap="mkgmap-r2179"
+splitter="splitter-r180"
 #nome della mappa
 serie=${name_nation}" creata da ital.img"
 #assegna il livello della mappa se sul dispositivo sono presenti più mappe
 priority="10"
 
 ##### SCRIP #####
-
-if [ "$prefix" = bz ] ; then
-
+function create()
+{
+if [ "$EXT" = bz2 ] ; then
   bzcat $file_in > ${name_nation}.osm
-
 fi
 
 ### CREA IL FILE DELLA NAZIONE ###
 #divide il file osm, se si cambia regione ricordarsi di cambiare il nome
-if [ "$prefix" = bz ] ; then
-  java -Xmx2500M -jar splitter-r180/splitter.jar --overlap=2000 ${name_nation}.osm
+if [ "$EXT" = bz -o "$EXT" = osm ] ; then
+  java -Xmx2500M -jar $splitter/splitter.jar --overlap=2000 ${name_nation}.osm
 else
-  java -Xmx2500M -jar splitter-r180/splitter.jar --overlap=2000 $file_in
+  java -Xmx2500M -jar $splitter/splitter.jar --overlap=2000 $file_in
 fi
 cd tmp/nations
 #crea il file img
-java -Xmx2000M -jar ../../mkgmap-r1995/mkgmap.jar --style-file=$style_it --net --route --latin1 --country-name="$name" --country-abbr="$abbr" --draw-priority=$priority --add-pois-to-areas --series-name="$serie" ../../6*.osm.pbf  #--style-file=$style
-java -Xmx1000M -jar ../../mkgmap-r1995/mkgmap.jar --gmapsupp *.img
+java -Xmx2000M -jar ../../$mkgmap/mkgmap.jar --style-file=$style_it --net --route --latin1 --country-name="$name" --country-abbr="$abbr" --draw-priority=$priority --add-pois-to-areas --series-name="$serie" ../../6*.osm.pbf  #--style-file=$style
+java -Xmx1000M -jar ../../$mkgmap/mkgmap.jar --gmapsupp *.img
 tar -cf ../../output_img/${name_nation}.tar gmapsupp.img ../../README_data.txt 
 gzip -9 -f ../../output_img/${name_nation}.tar
 cd ../../
 rm -rf tmp/nations*
+}
+#ciclo per vedere le opzioni scelte
+while getopts ":po" Opzione
+do
+    case $Opzione in
+        #scarica file pbf invece che bz2
+        p ) PBF=1;;
+        #scarica file pbf invece che bz2
+        o ) OSM=1;;        
+    esac
+done
+
+#controlla se scaricare bz2 o pbf
+if [ "$PBF" ] ; then
+    EXT=pbf
+elif [ "$OSM" ]; then
+    EXT=osm
+else
+    EXT=bz2
+fi
+
+create
 
 #rimuove tutti i file non più utili
 rm -rf 632400*
