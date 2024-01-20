21d78c21601d34bd5c09d5180e204e28dc42951a
Luca Delucchi <lucadeluge@gmail.com>
2024-01-20 07:23:33 +0100

    improved other nation script

--- other_nation.sh
+++ other_nation.sh
@@ -37,7 +37,7 @@ Opzioni:
 
 }
 #controlla se non ci sono parametri ed stampa l'help
-if [ $# -eq "$NO_ARG" ] 
+if [ $# -eq "$NO_ARG" ]
 then
     usage
     exit
@@ -46,13 +46,11 @@ fi
 if [ $# -eq "1" ]
 then
   file_in=$1;
-  name_nation=`echo $1 | cut -d'.' -f'1';`
-  prefix=`echo ${1##*.}`;
+  name_nation=`echo $1 | cut -d'.' -f'1' | rev | cut -d"/" -f"1" | rev;`
 elif [ $# -eq "2" ]
 then
   file_in=$2;
-  name_nation=`echo $2 | cut -d'.' -f'1';`
-  prefix=`echo ${2##*.}`;
+  name_nation=`echo $2 | cut -d'.' -f'1' | rev | cut -d"/" -f"1" | rev;`
 fi
 
 #####VARIABILI DA SETTARE#####
@@ -61,10 +59,10 @@ name=$name_nation
 #abbreviazione della zona
 #abbr="IT"
 #nome dello style
-style_it="../../styles/gfoss"
-style_escu="../../styles/hiking"
-mkgmap="mkgmap-r4140"
-splitter="splitter-r591"
+style_it="styles/gfoss"
+style_escu="styles/hiking"
+mkgmap="mkgmap-r4916"
+splitter="splitter-r653"
 #nome della mappa
 serie=${name_nation}" creata da ital.img"
 #assegna il livello della mappa se sul dispositivo sono presenti più mappe
@@ -80,18 +78,47 @@ fi
 ### CREA IL FILE DELLA NAZIONE ###
 #divide il file osm, se si cambia regione ricordarsi di cambiare il nome
 if [ "$EXT" = bz2 -o "$EXT" = osm ] ; then
-  java -Xmx2500M -jar $splitter/splitter.jar ${name_nation}.osm
+  java -Xmx${XMX} -jar ${MYPATH}/${splitter}/splitter.jar --max-areas=4096 --max-nodes=3000000 --wanted-admin-level=8 --geonames-file=cities15000.txt ${name_nation}.osm
 else
-  java -Xmx2500M -jar $splitter/splitter.jar $file_in
+  java -Xmx${XMX} -jar ${MYPATH}/${splitter}/splitter.jar --max-areas=4096 --max-nodes=3000000 --wanted-admin-level=8 --geonames-file=cities15000.txt $file_in
 fi
-cd tmp/nations
 #crea il file img
-java -Xmx2000M -jar ${MYPATH}/$mkgmap/mkgmap.jar --style-file=$style_it --net --route --latin1 --country-name="$name" --country-abbr="$abbr" --draw-priority=$priority --add-pois-to-areas --series-name="$serie" ${MYPATH}/6*.osm.pbf  #--style-file=$style
-java -Xmx1000M -jar ${MYPATH}/$mkgmap/mkgmap.jar --gmapsupp *.img
+
+java -Xmx${XMX} -jar ${MYPATH}/${mkgmap}/mkgmap.jar \
+    --style-file=$style_it \
+    --latin1 \
+    --country-name=Italia \
+    --country-abbr="$name_nation" \
+    --region-name="$name_nation" \
+    --area-name="$name_nation" \
+    --family-name="OpenStreetMap: $name_nation" \
+    --description="$name_nation" \
+    --precomp-sea=${MYPATH}/sea/ \
+    --generate-sea \
+    --bounds=${MYPATH}/bounds/ \
+    --max-jobs \
+    --route \
+    --drive-on=detect,right \
+    --process-destination \
+    --process-exits \
+    --location-autofill=is_in,nearest \
+    --index \
+    --split-name-index \
+    --housenumbers \
+    --add-pois-to-areas \
+    --link-pois-to-ways \
+    --preserve-element-order \
+    --verbose \
+    --name-tag-list=name,name:it,loc_name,reg_name,nat_name \
+    --draw-priority=$priority \
+    --reduce-point-density=3.2 \
+    --make-opposite-cycleways \
+    --keep-going \
+    --gmapsupp \
+    6*.osm.pbf
+
 tar -cf ${MYPATH}/output_img/${name_nation}.tar gmapsupp.img ${MYPATH}/README_data.txt
 gzip -9 -f ${MYPATH}/output_img/${name_nation}.tar
-cd ${MYPATH}
-rm -rf tmp/nations/*
 }
 #ciclo per vedere le opzioni scelte
 while getopts ":po" Opzione
@@ -118,8 +145,7 @@ echo $EXT
 create
 
 #rimuove tutti i file non più utili
-rm -rf 632400*
-rm -rf *.img 
-rm -rf *.IMG 
+# rm -rf 632400*
+# rm -rf *.img
+# rm -rf *.IMG
 # rm -rf *.osm
- 
